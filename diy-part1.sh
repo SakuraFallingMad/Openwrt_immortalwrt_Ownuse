91d55e965d53448ebff0682799d497db5a14efe1
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2022-06-18 14:28:22 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,9 +19,8 @@
 
 # Add prepareCompile
 disablePkgsList="
-./feeds/luci/applications/luci-app-argon-config
-./feeds/luci/themes/luci-theme-argon
 ./feeds/kenzo/luci-app-argon-config
+./feeds/kenzo/luci-app-argonne-config
 ./feeds/kenzo/luci-theme-argon
 ./feeds/kenzo/luci-theme-argonne
 "
