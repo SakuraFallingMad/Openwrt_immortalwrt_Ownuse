ca36bb840c23a7d6bf9eb81af36f29cf0b487c41
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2022-06-19 12:21:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,6 +24,7 @@ disablePkgsList="
 ./feeds/kenzo/luci-theme-argon
 ./feeds/kenzo/luci-theme-argonne
 ./feeds/luci/applications/luci-app-softethervpn
+./feeds/luci/themes/luci-theme-argon
 ./feeds/packages/net/adguardhome
 "
 
